e391c47139d18b806af7afaaf55e864212323881
Benji Speer <benjibuiltit@gmail.com>
2024-10-16 10:28:33 -0400

    configure jwt secret

--- backend/app/models/concerns/json_web_token.rb
+++ backend/app/models/concerns/json_web_token.rb
@@ -1,7 +1,7 @@
 require "jwt"
 module JsonWebToken
   extend ActiveSupport::Concern
-  SECRET_KEY = Rails.application.credentials.secret_key_base
+  SECRET_KEY = ENV["JWT_SECRET_KEY"]
 
   def jwt_encode(payload, exp = 7.days.from_now)
     payload[:exp] = exp.to_i
